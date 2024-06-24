5e8f4b8c4c74b9430e52cc6abbbf7d5d42073a77
badasswp <badasswpdev@gmail.com>
2024-06-24 17:44:38 +0100

    chore: set up plugin file

---
+++ quick-dry-login.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Quick & Dry Login
+ * Plugin URI:  https://github.com/badasswp/quick-dry-login
+ * Description: Manually select any user profile to log into your WP development or staging website.
+ * Version:     1.0.0
+ * Author:      badasswp
+ * Author URI:  https://github.com/badasswp
+ * License:     GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: quick-dry-login
+ * Domain Path: /languages
+ *
+ * @package QuickDryLogin
+ */
+
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
