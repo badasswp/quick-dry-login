17b375c99dcaaa89904e87cd2214f027472d7c95
badasswp <badasswpdev@gmail.com>
2024-06-28 00:07:42 +0100

    chore: add doc block to JS script

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+/**
+ * Event Listener: ID - 'quick-dry-select'.
+ * 
+ * Hit WP REST endpoint with the selected user ID value, if successful,
+ * redirect the user to admin page, authenticate session.
+ *
+ * @event change
+ * @param {Event} evt - The event object.
+ */
 document.getElementById('quick-dry-select').addEventListener('change',
 	async function (evt) {
 		if (!this.value) {
